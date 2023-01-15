4d574f6c944420f635d11626f9802850178cfa5d
LadyRit <111931701+LadyRit@users.noreply.github.com>
2023-01-15 06:18:36 -0500

    Updated App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Aws and Jenkins!" );
+        System.out.println( "Hello World from Aws and Jenkins! What is going on" );
     }
 }
